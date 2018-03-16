5221d474cf4f24d86bfc5844c3a93e22e763ecb1
Matias Montes <matias@semexpert.com.ar>
2018-03-16 10:19:18 -0300

    Prueba de insertar a Dynamo

--- JobBroker/index.js
+++ JobBroker/index.js
@@ -7,6 +7,8 @@ const SAP_ORDER_QUEUE_URL = process.env.SAP_ORDER_QUEUE_URL;
 const AWS_REGION = process.env.AWS_REGION;
 
 const sqs = new AWS.SQS({ region: AWS_REGION });
+const db = new AWS.DynamoDB();
+
 
 exports.handler = function (event, context, callback) {
     console.log('sendMessageToSapOrderQueue');
@@ -36,6 +38,25 @@ exports.handler = function (event, context, callback) {
         };
     };
 
+    const saveInDb = (message) => {
+        return () => {
+            const params = {
+                Item: {
+                    order_id: {
+                        S: '123'
+                    },
+                    payload: {
+                        S: 'a very long json string'
+                    }
+                },
+                TableName: 'Order',
+                ConditionExpression: "attribute_not_exists(order_id)"
+            };
+
+            return db.putItem(params).promise();
+        };
+    };
+
     let processSingleMessage = message => {
         return new Promise((resolve, reject) => {
             const messageBody = JSON.parse(message.Body);
@@ -48,6 +69,7 @@ exports.handler = function (event, context, callback) {
                     case 'order':
                         sendMessageToSapOrderQueue(messageBody.payload)
                             .then(deleteFromJobQueue(message))
+                            .then(saveInDb(message))
                             .then(Promise.resolve("Done " + message.MessageId))
                             .catch(err => resolve(err));
 
