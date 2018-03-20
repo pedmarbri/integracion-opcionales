b9bae2a0563ed90c7ab6db61b6467e1a2c2b4c21
Matias Montes <matias@semexpert.com.ar>
2018-03-20 00:40:53 -0300

    No pinchan los tests de Jasmine

--- JobBroker/spec/job-queue-service.spec.js
+++ JobBroker/spec/job-queue-service.spec.js
@@ -1,11 +1,44 @@
 'use strict';
 
 const sinon = require( 'sinon' );
-const jobQueueService = require('../job-queue-service');
+const proxyquire = require( 'proxyquire' ).noCallThru();
 
 describe('Job Queue Service', () => {
-    it('Uses promises on receiveMessage', () => {
-        const request = jobQueueService.receiveMessages();
-        expect(request).toEqual(jasmine.any(Promise));
+
+    let jobQueueService;
+    let sqsStub;
+    let sqsRequestStub;
+    let AWSStub;
+    let stubConfig;
+
+    beforeEach(() => {
+        sqsStub = {
+            receiveMessage: sinon.stub()
+        };
+
+        sqsRequestStub = {
+            promise: () => Promise.resolve([])
+        };
+
+        sqsStub.receiveMessage.returns(sqsRequestStub);
+
+        AWSStub = {
+            SQS: sinon.stub().returns(sqsStub)
+        };
+
+        stubConfig = {
+            'aws-sdk': AWSStub
+        };
+
+        jobQueueService = proxyquire('../job-queue-service', stubConfig);
+    });
+
+    it('Returns a promise on receiveMessage', () => {
+        const messagesPromise = jobQueueService.receiveMessages();
+        expect(messagesPromise).toEqual(jasmine.any(Promise));
+    });
+
+    xit('Contains json messages', () => {
+
     });
 });
