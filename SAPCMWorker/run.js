6e4a82983e184a01e98b616a69b0d6a4a07922a0
Matias Montes <matias@semexpert.com.ar>
2018-04-13 10:43:33 -0300

    Script para hacer pruebas de NC

--- SAPCMWorker/run.js
+++ SAPCMWorker/run.js
@@ -3,28 +3,35 @@
 process.env.LN_STACK = 'Test';
 const lambda = require('./index');
 
+//0500000025
+
 lambda.handler(
     {
         Body: JSON.stringify({
-            "creditmemo_id": "70800708",
-            "order_id": "{{$randomInt}}00{{$randomInt}}",
-            "timestamp": "2018-03-16T20:03:15Z",
+            "creditmemo_id": "12700100402",
+            "order_id": "12700100002",
+            "sap_order_id": "0500000028",
+            "timestamp": "2018-04-10T14:59:00Z",
             "totals": {
-                "shipping": 35
+                "shipping": 0
             },
             "items": [
                 {
-                    "sku": "colecciones_978841612704700011-UN-UN",
-                    "qty": 1,
+                    "sku": "OPC11065300060",
+                    "name": "VOLVO-G940",
+                    "qty": 2,
                     "refund_amount": 199.9,
-                    "row_total": 199.9
+                    "row_total": 399.8,
+                    "sap_row": 20,
+                    "discount_amount": 19.99,
+                    "discount_percent": 10
                 }
             ]
         })
     },
     {},
     function (err, result) {
-        console.error(err);
-        console.log(result);
+        console.error(JSON.stringify(err));
+        console.log(JSON.stringify(result));
     }
 );
