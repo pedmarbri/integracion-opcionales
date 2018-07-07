97438b1b0c823dce7969fb0cae93380edde6c920
Matias Montes <matias@semexpert.com.ar>
2018-07-07 20:21:25 -0300

    Restricciones de tamaños de campos para los servicios de SAP

--- functions/SAPOrderWorker/spec/sap-service.spec.js
+++ functions/SAPOrderWorker/spec/sap-service.spec.js
@@ -9,11 +9,10 @@ function setupServiceMocks(clientStub, expectedRequest, sampleResponse) {
      */
     const clientMock = sinon.mock(clientStub);
 
-    const soapMethodExpectation = clientMock.expects('ZWS_GEN_PEDAsync')
-        .once()
-        .withArgs(expectedRequest)
-        .resolves(sampleResponse);
-    return soapMethodExpectation;
+  return clientMock.expects('ZWS_GEN_PEDAsync')
+      .once()
+      .withArgs(expectedRequest)
+      .resolves(sampleResponse);
 }
 
 describe('Sap Service', () => {
@@ -204,5 +203,114 @@ describe('Sap Service', () => {
         SapService.sendOrder(sampleOrder)
             .then(() => soapMethodExpectation.verify())
             .catch(fail);
+    });
+
+    it('Truncates fields to its maximum length in SAP', () => {
+      const soapMethodExpectation = setupServiceMocks(clientStub, expectedRequest, sampleResponse);
+      const longString = '1234567890'.repeat(100);
+      const bigNumber = Number.MAX_VALUE;
+
+      const matchNumber = maxDigits => {
+          const maxValue = Math.pow(10, maxDigits);
+          return sinon.match.number.and(sinon.match(value => value < maxValue));
+      };
+
+      const matchString = maxLength => {
+          return sinon.match.string.and(sinon.match(value => value.length <= maxLength));
+      };
+
+      const conditionExpectation = function () {
+        return {
+          KPOSN: sinon.match.typeOf("null").or(matchNumber(6)),
+          KSCHL: matchString(4),
+          KBETR: sinon.match(matchNumber(28)),
+          WAERS: sinon.match.typeOf("null").or(matchString(5))
+        };
+      };
+
+      sampleOrder.order_id = longString;
+      sampleOrder.shipping_method = longString;
+      sampleOrder.sales_channel = longString;
+      sampleOrder.customer.email = longString;
+      sampleOrder.customer.first_name = longString;
+      sampleOrder.customer.last_name = longString;
+      sampleOrder.customer.id_type = longString;
+      sampleOrder.customer.id_number = longString;
+      sampleOrder.customer.gender = longString;
+      sampleOrder.totals.subtotal = bigNumber;
+      sampleOrder.totals.discount = bigNumber;
+      sampleOrder.totals.shipping = bigNumber;
+      sampleOrder.totals.finance_cost = bigNumber;
+      sampleOrder.totals.grand_total = bigNumber;
+      sampleOrder.billing_address.country = longString;
+      sampleOrder.billing_address.region = longString;
+      sampleOrder.billing_address.city = longString;
+      sampleOrder.billing_address.post_code = longString;
+      sampleOrder.billing_address.street = longString;
+      sampleOrder.billing_address.number = bigNumber;
+      sampleOrder.billing_address.floor = longString;
+      sampleOrder.billing_address.apartment = longString;
+      sampleOrder.billing_address.first_name = longString;
+      sampleOrder.billing_address.last_name = longString;
+      sampleOrder.billing_address.email = longString;
+      sampleOrder.billing_address.telephone = longString;
+      sampleOrder.shipping_address.country = longString;
+      sampleOrder.shipping_address.region = longString;
+      sampleOrder.shipping_address.city = longString;
+      sampleOrder.shipping_address.post_code = longString;
+      sampleOrder.shipping_address.street = longString;
+      sampleOrder.shipping_address.number = bigNumber;
+      sampleOrder.shipping_address.floor = longString;
+      sampleOrder.shipping_address.apartment = longString;
+      sampleOrder.shipping_address.first_name = longString;
+      sampleOrder.shipping_address.last_name = longString;
+      sampleOrder.shipping_address.email = longString;
+      sampleOrder.shipping_address.telephone = longString;
+      sampleOrder.items[0].sku = longString;
+      sampleOrder.items[0].name = longString;
+      sampleOrder.items[0].qty = bigNumber;
+      sampleOrder.items[0].weight = bigNumber;
+      sampleOrder.items[0].row_weight = bigNumber;
+      sampleOrder.items[0].list_price = bigNumber;
+      sampleOrder.items[0].discount_percent = bigNumber;
+      sampleOrder.items[0].discount_amount = bigNumber;
+      sampleOrder.items[0].row_total = bigNumber;
+      sampleOrder.payment.method = longString;
+      sampleOrder.payment.transaction_id = longString;
+
+      expectedRequest.AD_SMTPADR = matchString(241);
+      expectedRequest.AUART = matchString(4);
+      expectedRequest.AUGRU = matchString(3);
+      expectedRequest.BSTDK = matchString(8);
+      expectedRequest.BSTKD = matchString(35);
+      expectedRequest.CITY = matchString(40);
+      expectedRequest.COUNTRY = matchString(3);
+      expectedRequest.IHREZ = matchString(12);
+      expectedRequest.KUNNR = matchString(10);
+      expectedRequest.LANGU = matchString(1);
+      expectedRequest.NAME1 = matchString(30);
+      expectedRequest.NAME4 = matchString(30);
+      expectedRequest.SPART = matchString(2);
+      expectedRequest.VBELN_EXT = sinon.match.typeOf("null");
+      expectedRequest.VKORG = matchString(4);
+      expectedRequest.VTWEG = matchString(2);
+      expectedRequest.T_ITEMS.item[0].POSNR = matchNumber(6);
+      expectedRequest.T_ITEMS.item[0].MATNR = matchString(18);
+      expectedRequest.T_ITEMS.item[0].WERKS = matchString(4);
+      expectedRequest.T_ITEMS.item[0].LGORT = matchString(4);
+      expectedRequest.T_ITEMS.item[0].MENGE = matchNumber(13);
+      expectedRequest.T_ITEMS.item[0].MEINS = matchString(3);
+      expectedRequest.T_ITEMS.item[0].MVGR5 = matchString(3);
+      expectedRequest.T_ITEMS.item[0].KDMAT = matchString(35);
+      expectedRequest.T_ITEMS.item[0].POSEX = matchString(6);
+      expectedRequest.T_CONDITIONS.item[0] = conditionExpectation();
+
+      // Add a condition for the discount and another for the shipping
+      expectedRequest.T_CONDITIONS.item[1] = conditionExpectation();
+      expectedRequest.T_CONDITIONS.item[2] = conditionExpectation();
+
+      SapService.sendOrder(sampleOrder)
+        .then(() => soapMethodExpectation.verify())
+        .catch(fail);
     });
 });
